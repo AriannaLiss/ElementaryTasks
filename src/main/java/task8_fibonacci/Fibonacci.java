446e6736696b17b76518eb6b58b3ffa328acd6b8
Lidia Kalakutskaya <lidia.kalakutskaya@gmail.com>
2021-03-09 17:50:55 +0200

    Task 8 Fibonacci Row
     * Show all Fibonacci's numbers, that fit to condition:
     * numbers are in the range (you should input positive 'from' 'to' numbers in a range [0..2147483647]);
     * Fibonacci's row with fixed length numbers (from 1 to 9).
    
    Added isPositiveInt() and isIntInRange() to general/Validator

---
+++ src/main/java/task8_fibonacci/Fibonacci.java
@@ -0,0 +1,105 @@
+package task8_fibonacci;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fibonacci {
+    private List<Integer> fibonacciRow;
+
+    Fibonacci(){
+        fibonacciRow = new ArrayList<Integer>();
+        init(0,1);
+    }
+
+    private int init (int firstNumber, int secondNumber){
+        init();
+        fibonacciRow.add(firstNumber);
+        fibonacciRow.add(secondNumber);
+        return firstNumber+secondNumber;
+    }
+
+    //only one number could be in the row
+    private int init (int number){
+        init();
+        fibonacciRow.add(number);
+        return 0;
+    }
+
+    //no numbers in the row
+    private int init(){
+        fibonacciRow.clear();
+        return 0;
+    }
+
+    //fibonacci should has at least two numbers!
+    private int getNextNumber(){
+        return fibonacciRow.get(fibonacciRow.size()-1)+fibonacciRow.get(fibonacciRow.size()-2);
+    }
+
+    //make fibonacci with first two numbers greater or equal value from
+    //returns third fibonacci number or 0 if there are only 1 or no numbers in the row
+    private int startFibonacci(int from) {
+        if (from == 0) {
+            return init(0, 1);
+        }
+        if (from == 1) {
+            return init(1, 1);
+        }
+        int f0 = 0;
+        int f1 = 1;
+        int f = f0 + f1;
+        while ((f < from)&&(f>0)) {
+            f = f0 + f1;
+            f0 = f1;
+            f1 = f;
+        }
+        if (f<0){
+            return init();
+        }
+        int f2 = f + f0;
+        if (f2<0){
+            return init (f);
+        }
+        return init(f, f2);
+    }
+
+    //from>=0 and to>=0
+    //from and to < 1000000000
+    public List<Integer> getFibonacciRow(int from, int to){
+        if (from>to){
+            int buf = from;
+            from=to;
+            to=buf;
+        }
+        int f = startFibonacci(from);
+        if ((fibonacciRow.size()>1)&&(to>0)) {
+            while ((f <= to)&&(f>0)) {
+                fibonacciRow.add(f);
+                f = getNextNumber();
+            }
+        }
+        //remove if 'to' less then last numbers in the row
+        removeExtra(to);
+        return fibonacciRow;
+    }
+
+    private void removeExtra(int higherBound){
+        while ((fibonacciRow.size()>0)&&(fibonacciRow.get(fibonacciRow.size()-1)>higherBound)){
+            fibonacciRow.remove(fibonacciRow.size()-1);
+        }
+    }
+
+    //Requirements: (countOfDigits>0)&&(countOfDigits<10)
+    //if countOfDigits == 1 row will start from 1
+    //example: [1, 1, 2, 3, 5, 8]
+    //return fibonacci row with numbers witch have countOfDigits digits
+    public List<Integer> getFibonacciRow(int countOfDigits){
+        int from = (int)Math.pow(10,countOfDigits - 1);
+        return getFibonacciRow(from, from*10-1);
+    }
+
+    @Override
+    public String toString() {
+        return fibonacciRow.toString();
+    }
+}
