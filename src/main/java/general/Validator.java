0a5bd90b0c7ad39b3df217bd03febb549e9f5d31
Lidia Kalakutskaya <lidia.kalakutskaya@gmail.com>
2021-03-09 17:53:12 +0200

    Modified general/Validator
    Added isPositiveInt() and isIntinRange().
    Removed isGreaterZero().

--- src/main/java/general/Validator.java
+++ src/main/java/general/Validator.java
@@ -11,7 +11,18 @@ public class Validator {
         return arg.matches("-?\\d+");
     }
 
-    public static boolean isGreaterZero(double value) {
-        return (value > 0);
+    //could be equal zero
+    public static boolean isPositiveInt(String arg){
+        return arg.matches("\\d+");
+    }
+
+    //return true if param is integer and fits to range [from..to]
+    public static boolean isIntInRange(String param, int from, int to){
+        if(isInt(param))
+        {
+            int value = Integer.parseInt(param);
+            return (value >= from)&&(value <= to);
+        }
+        return false;
     }
 }
